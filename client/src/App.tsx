86c209d8d0e650e4c5cab49d27f9acc412327811
Anton Krotyuk <a.krotyuk@gmail.com>
2022-12-18 17:53:07 +0000

    General app structure setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/system";
+import React, { useState } from "react";
+import { Provider } from "react-redux";
+import { useTheme } from "@mui/material";
 
-function App() {
+import store from "./store/store";
+import "./app.css";
+import Router from "./components/Router";
+
+const appTheme = createTheme();
+
+const App: React.FunctionComponent = () => {
+  const [startUpActionsAreDone, setStartUpActionsAreDone] = useState(false);
+  const theme = useTheme();
+  const spinnerStyle = {
+    borderColor: `${theme.palette.info.dark} transparent ${theme.palette.info.dark} transparent`,
+  };
+
+
   return (
-    <div className="App">
-      <h1>THE APP IS GOING TO BE HERE ONE DAY</h1>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={appTheme}>
+        <div
+          className="app"
+          style={{ backgroundColor: theme.palette.info.light }}
+        >
+          {startUpActionsAreDone ? (
+            <Router />
+          ) : (
+            <div className="animation-container">
+              <div className="spinner" style={spinnerStyle}></div>
+            </div>
+          )}
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Router.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Router: React.FunctionComponent = () => {
+  return <>THE APP IS GOING TO BE HERE ONE DAY</>;
+};
+
+export default Router;
